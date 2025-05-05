75c9517d0b1cc35f361320dea5810d67b8f6303a
Lu23cas07 <sismanmagalhaeslucas@gmail.com>
2025-05-05 15:22:54 -0300

    Add files via upload

---
+++ ativ 6.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=0;//declarar que variavel comeca em 0
+    do{
+        printf("i: %d\n", i);
+        i +=10;//pular de um em um ate 100
+    }while(i <= 100 );//repetir
+return 0;//finalização
+}
